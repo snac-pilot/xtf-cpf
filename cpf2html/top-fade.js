651db6bd759a5fc7646fa7d6a0487e0766551fba
Brian Tingle <tingletech@users.noreply.github.com>
2010-10-23 00:54:33 -0400

    had to externalize b/c inline > in xslt is too hard; should rename when js is all moved to external file?

---
+++ cpf2html/top-fade.js
@@ -0,0 +1,18 @@
+ $(function () {        /* http://agyuku.net/2009/05/back-to-top-link-using-jquery/ */
+     $(window).scroll(function () {  
+         var f = $(this).scrollTop();
+         if ( $(this).scrollTop() > 2500 ) {         
+             $('.return-top').fadeIn();
+         } else {
+             $('.return-top').fadeOut();
+         }
+     });
+     $('.return-top').click(function () {
+         $('body,html').animate({
+             scrollTop: 0
+         },
+         800);
+         _gaq.push(['snak._trackEvent', 'OnPage', 'BackToTop', window.title ]);
+         return false;
+     });
+ });
